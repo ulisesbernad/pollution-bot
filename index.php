49acb7d1494a8fd3685016e5794ee3b17b3ac654
Ulises <ulises@pc>
2020-01-12 19:07:25 +0100

    New format and planning examples

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-
+$city='berlin';
+#Format city must be lowercase
+#
 echo "This is Telegram Polution BOT";
+
+#Example QUERY
+#
+#We must change madrid for $city variable for get the data.
+#
+#http://api.waqi.info/feed/madrid/?token=b340407b2e6e2ffda796bfc6633093303a965ece
+#
+
 ?>
