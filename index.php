37d837d8dd90e808d69e09880d3ce2b299699394
Ulises <ulises@pc>
2020-01-12 20:36:45 +0100

    Gettig some data from the API fist test

--- index.php
+++ index.php
@@ -10,5 +10,15 @@ echo "This is Telegram Polution BOT";
 #
 #http://api.waqi.info/feed/madrid/?token=b340407b2e6e2ffda796bfc6633093303a965ece
 #
+// Crea un nuevo recurso cURL
+$ch = curl_init();
 
+// Establece la URL y otras opciones apropiadas
+curl_setopt($ch, CURLOPT_URL, "http://api.waqi.info/feed/madrid/?token=b340407b2e6e2ffda796bfc6633093303a965ece");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// Captura la URL y la envía al navegador
+curl_exec($ch);
+
+die (curl_exec($ch));
 ?>
